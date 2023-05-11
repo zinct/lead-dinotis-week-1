ac34db7e29b5cbbf1c44a0364d2f16a96f70f1bd
Indra Mahesa <indramahesa128@gmail.com>
2023-05-11 20:45:18 +0700

    Initial Commit

---
+++ Lead Dinotis Week 1/Lead_Dinotis_Week_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Lead_Dinotis_Week_1App.swift
+//  Lead Dinotis Week 1
+//
+//  Created by Indra Mahesa on 11/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Lead_Dinotis_Week_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
